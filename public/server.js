7f00c4bd2dca6b09aa7cf03cb053d234839a261f
Erik Stone <spologas@gmail.com>
2021-08-10 15:49:52 -0700

    added server file, not sure if this is going to help get/post notes, save button still not appearing

---
+++ public/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+res.send(`It's Alive! path Hit ${req.url}`);
+});
+
+app.get('/api/notes', (req, res) => {
+    req.JSON.parse('./api/assets/notes')
+    // how do I post this in the left hand side of the notes.html?
+    res.send('../db/db.json')
+})
+
+app.post('/api/notes', (req, res) => {
+    res.send.json('notes')
+})
+
+
+
+const server = http.createServer(handleRequest)
+
+app.listen(PORT, () => {
+console.log(`server listening on: http//localhost:${PORT}`);
+});
