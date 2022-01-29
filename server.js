bde29edd11b25ea72055870908f47b925de94e1f
Erik Stone <spologas@gmail.com>
2022-01-29 14:51:10 -0800

    added app.disable('etage'), removed 304 but no update of deleted note.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(('public')));
 
+// app.disable('etag');
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
